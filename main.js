72e74af53ed62c84e2ed9e7bd9e078f5eb2c9ed1
SIsiam <shahidulmd392@gmail.com>
2021-02-08 22:02:47 +0600

    modifires When I got a extra Time

--- main.js
+++ main.js
@@ -1,115 +1,81 @@
-// First step : ES6 Variable declare with querySelector class target 
+const searchFood = () => {
+    const userSearch = document.getElementById('search-box').value;
+    const myUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${userSearch}`
 
-const mainUrl = "https://www.themealdb.com/api/json/v1/1/";
-const inputSearch = document.querySelector(".input-search");
-const searchButton = document.querySelector(".search-button");;
-const mealsContainerDisp = document.querySelector(".Meals-container");
-const containersDetails = document.querySelector(".Meals-details");;
-
-
-// Second Step : When Users search button click Then Display this Outpoot code is here
-
-
-searchButton.addEventListener("click", () => MealSearch(inputSearch.value));
-
-// Search Food name or query
-const MealSearch = query => {
-    if (query != "") {
-        searchMsg(mealsContainerDisp, true);
-
-        const url = `${mainUrl}search.php?s=${query}`;
-
-        fetch(url)
-            .then(data => data.json())
-            .then(data => {
-                searchMsg(mealsContainerDisp, false);
-                displayMeals(data);
-            });
-    } else {
-        mealsContainerDisp.innerHTML = "";
-    }
-}
-
-// step-3 : 
-
-const displayMeals = data => data.meals != null ? mealsContainerDisp.innerHTML = createDiv(data) : notMatchingAlert();
-
-const createDiv = data => {
-
-    const sectionMeals = data.meals;
-
-    let str = "";
-    sectionMeals.forEach(data => {
-
-        str = str + `<div class="food-item" onclick="divClickd(${data.idMeal})">
-
-                        <div class="every-Meals">
-                            <img src="${data.strMealThumb}"/>
-                             <h3 class = f-name>${data.strMeal}</h3>
-                              </div>
-
-                         </div>`;
-
-    });
-
-    return str;
-}
-
-
-
-
-// step-4: Meal Details if You any Select Food
-
-const divClickd = id => {
-
-    const myUrl = `${mainUrl}lookup.php?i=${id}`;
+    // My Food Loading Data
 
     fetch(myUrl)
-
-        .then(data => data.json())
-        .then(data => {
-
-            const foodDetails = data.meals[0];
-
-            containersDetails.innerHTML = `
-
-            <div class ="clicked-Div-details">
-
-                    <div class = first-div>
-                       <h4> ${foodDetails.strMeal} </h4>
-                      <img class="card-img-top details-image" src="${foodDetails.strMealThumb}">
-                     </div>
-
-                     <div class="card-body second-div">
-                            <h4> The ingredients </h4>
-                            <li> <i class="fas fa-check-square fa-2x"> ${foodDetails.strIngredient1} </i> </li>
-                            <li> <i class="fas fa-check-square fa-2x"> ${foodDetails.strIngredient2} </i> </li>
-                            <li> <i class="fas fa-check-square fa-2x"> ${foodDetails.strIngredient3} </i> </li>
-                            <li> <i class="fas fa-check-square fa-2x"> ${foodDetails.strIngredient4} </i> </li>
-                            <li> <i class="fas fa-check-square fa-2x"> ${foodDetails.strMeasure1} </i> </li>
-                            <li> <i class="fas fa-check-square fa-2x"> ${foodDetails.strMeasure2} </i> </li>
-                            <li> <i class="fas fa-check-square fa-2x"> ${foodDetails.strMeasure3} </i> </li>
-                            <li> <i class="fas fa-check-square fa-2x"> ${foodDetails.strMeasure4} </i> </li>
-                      </div>
-            </div>
-         `;
-        });
+        .then(respo => respo.json())
+        .then(myJsonData => displayMeal(myJsonData.meals))
+        .catch(error => displayError('Oppps!! Please try again later!'));
 }
 
-/* When Display search True Or false Then Show This Outpoot.
-1.If arguments false Then show not matching massage. 
-2.But when The outpoot is True then work This Code  */
 
-const searchMsg = (div, order) => order ? div.innerHTML = `<div></div>` : "";
+const displayMeal = MyFood => {
 
+    const foodContainer = document.getElementById('Meals-container');
+    foodContainer.innerHTML = '';
+    MyFood.forEach(food => {
+        const everyFoodDiv = document.createElement('div');
+        everyFoodDiv.className = 'single-result row align-items-center my-3 p-3 song-div-style';
+        everyFoodDiv.innerHTML = `
 
-
-
-
-// last Step : Not matching any Meal Then User recommend Alert and Suggest msg for Right Word search
-const notMatchingAlert = () => mealsContainerDisp.innerHTML = `
-            <div class = not-match>
-                <h2>Opps! Try Again Please</h2>
-                <h5> Suggest This Word: S, sea , A a B, b</h5>
+            <div class="col-md-9">
+            <img onclick = "ingredients('${food.idMeal}')" src="${food.strMealThumb}" width="200">
             </div>
+            <div class="col-md-9">
+                <h2>${food.strMeal}</h2>
+            </div>
+           
             `;
+        foodContainer.appendChild(everyFoodDiv);
+    })
+}
+
+
+// Food Click when User Click anyFood item 
+
+const ingredients = async (id) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+
+    console.log(url);
+
+    try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displaySelectedFood(data.meals[0]);
+    }
+    catch (error) {
+        displayError('Sorry! I failed to load lyrics, Please try again later!!!')
+    }
+
+}
+
+const displaySelectedFood = selectFood => {
+    const ingredientsDiv = document.getElementById('food-details');
+    ingredientsDiv.innerHTML = '';
+    ingredientsDiv.className = "select"
+    ingredientsDiv.innerHTML = `
+                <div class="col-md-9">
+                <h2>${selectFood.strMeal}</h2>
+                <img src="${selectFood.strMealThumb}" width="300">
+                </div>
+                <div class="card-body second-div">
+                <h4> The ingredients </h4>
+                <li> <i class="fas fa-check-square fa-2x"> ${selectFood.strIngredient1} </i> </li>
+                <li> <i class="fas fa-check-square fa-2x"> ${selectFood.strIngredient2} </i> </li>
+                <li> <i class="fas fa-check-square fa-2x"> ${selectFood.strIngredient3} </i> </li>
+                <li> <i class="fas fa-check-square fa-2x"> ${selectFood.strIngredient4} </i> </li>
+                <li> <i class="fas fa-check-square fa-2x"> ${selectFood.strMeasure1} </i> </li>
+                <li> <i class="fas fa-check-square fa-2x"> ${selectFood.strMeasure2} </i> </li>
+                <li> <i class="fas fa-check-square fa-2x"> ${selectFood.strMeasure3} </i> </li>
+                <li> <i class="fas fa-check-square fa-2x"> ${selectFood.strMeasure4} </i> </li>
+          </div>
+    `
+
+}
+
+const displayError = error => {
+    const errorMsg = document.getElementById('err-Msg');
+    errorMsg.innerText = error;
+}
